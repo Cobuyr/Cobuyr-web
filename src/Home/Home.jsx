c701490732758b6d3cadd495dd6fa526508ea751
zhaqyy <zaqrashyy7@gmail.com>
2023-11-10 14:16:17 +0100

    init

---
+++ src/Home/Home.jsx
@@ -0,0 +1,11 @@
+import './Home.css'
+
+function Home(){
+<section className='hero'>
+<h1 className='header-text'>
+MODERNIZE YOUR CHECKOUT FOR TODAY’S ERA OF SHARING WITH SOCIAL BUYING
+</h1>
+</section>
+}
+
+export default Home
