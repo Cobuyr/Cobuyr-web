4af23299ceb324a234dbbffa089631338b8a3804
zhaqyy <zaqrashyy7@gmail.com>
2023-11-12 13:56:58 +0100

    -rearranged directory -add base design to info banner section -added image to modern section

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import Home from './Pages/Home/Home'
+import Home from './Pages/Home'
 
 function App() {
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,12 @@
-import "./Home.css";
+import "../Styles/Home/Home.css";
+import dash from "/dash.jpg";
 
 const Home = () => {
   return (
     <>
     <section className="hero">
       <div className="badge">Welcome</div>
-      <h1 className="hero-h1">
+      <h1>
         MODERNIZE YOUR CHECKOUT FOR TODAY’S ERA OF SHARING WITH SOCIAL BUYING
       </h1>
       <h2 className="hero-h2">
@@ -19,8 +20,8 @@ const Home = () => {
     <div className="badge">The Future</div>
     <div className="future-wrap">
         <div className="future-textbox">
-            <h2 className="future-h2">THE FUTURE OF PAYMENTS</h2>
-            <p className="future-text">Unlock new sales and savings by providing your customers with the ability to purchase together with Social Buying, the future of online payments, from Cobuyr.</p>
+            <h2>THE FUTURE OF PAYMENTS</h2>
+            <p>Unlock new sales and savings by providing your customers with the ability to purchase together with Social Buying, the future of online payments, from Cobuyr.</p>
         </div>
         <div className="future-listbox">
             <ul className="listbox">
@@ -39,7 +40,7 @@ const Home = () => {
     <section className="transform">
     <div className="badge">Checkout Transformation</div>
         <div className="transform-textbox">
-            <h2 className="transform-h2">TRANSFORM YOUR CHECKOUT PROCESS FOR TODAY'S ERA OF SHARING </h2>
+            <h2>TRANSFORM YOUR CHECKOUT PROCESS FOR TODAY'S ERA OF SHARING </h2>
             <p className="transform-text">Cobuyr gives customers a new and memorable social buying experience that also modernizes your checkout process by making co-buying a reality, and creating more value for you.</p>
         </div>
         <div className="hex-box">
@@ -56,11 +57,25 @@ const Home = () => {
     </section>
 
     <section className="info-banner">
-    <div className="banner-textbox">
-            <h2 className="banner-h2">TRANSFORM YOUR CHECKOUT PROCESS FOR TODAY'S ERA OF SHARING </h2>
+        <div className="glow"></div>
+    <div className="textbox">
+            <h2>TRANSFORM YOUR CHECKOUT PROCESS FOR TODAY'S ERA OF SHARING </h2>
             <p className="banner-text">Cobuyr gives customers a new and memorable social buying experience that also modernizes your checkout process by making co-buying a reality, and creating more value for you.</p>
+            <button className="primary-btn">Book Demo</button>
+        </div>
+        <div className="glow2"></div>
+    </section>
+
+    <section className="modern">
+    <div className="textbox">
+            <h2>Customers love to share so why does your checkout stop them from spending more money with you? </h2>
+            <p>Payment technology has not kept up with what customers want, and that is why we invented Social Buying, a new category of multi-party payment transactions that enables your customers to split and securely pay for purchases together.</p>
+            <div className="image">
+                 <img src={dash} alt="new category of multi-party payment transactions" />
+            </div>
         </div>
     </section>
+
     </>
   );
 };
