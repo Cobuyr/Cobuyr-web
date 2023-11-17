a5f097976d5c999a53d8cd1c302308ccf5e4f2f2
zhaqyy <zaqrashyy7@gmail.com>
2023-11-17 15:18:35 +0100

    finsihed integration section -started new modern section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,50 +6,91 @@ const Home = () => {
     <>
       <section className="hero">
         <div className="badge">Welcome</div>
-        <h1>
-          MODERNIZE YOUR CHECKOUT FOR TODAY’S ERA OF SHARING WITH SOCIAL BUYING
-        </h1>
-        <h3 className="hero-h3">
-          Acquire <span className="highlight">more customers</span>,{" "}
-          <span className="highlight">increase average order</span> value and
-          drive <span className="highlight">repeat purchases</span> by enabling
-          your customers to buy together.
-        </h3>
+        <h1>Revolutionize Checkout with Social Buying</h1>
+        <p>
+          Enhance your checkout experience with Cobuyr's cutting-edge Social
+          Buying platform. Allow customers to{" "}
+          <span className="highlight">
+            purchase together, boosting sales and fostering a sense of
+            community.
+          </span>
+        </p>
         <button className="primary-btn">Join Waitlist</button>
       </section>
 
       <section className="future">
-        <div className="future-wrap">
-          <div className="future-textbox">
+        {/* <div className="future-wrap"> */}
+        <div className="future-textbox">
           <div className="badge">The Future</div>
-            <h2>THE FUTURE OF PAYMENTS</h2>
-            <p>
-              Unlock new sales and savings by providing your customers with the
-              <span className="highlight"> ability to purchase together with Social Buying</span>, the future of
-              online payments, from Cobuyr.
-            </p>
-          </div>
-          <div className="future-listbox">
-            <ul className="listbox">
-              <li>Lower the cost of customer acquisition</li>
-              <li>Highly personalised buying experience</li>
-              <li>Fewer discounts to move unsold products</li>
-              <li>Unique insights into customer networks</li>
-              <li>Multiple customer buy together in one order</li>
-              <li>Richer data for better marketing engagement</li>
-            </ul>
-          </div>
+          <h2>THE FUTURE OF PAYMENTS</h2>
+          <p>
+            Unleash the potential of your online business with Cobuyr's Social
+            Buying solution. This innovative platform empowers customers to pool
+            purchases,{" "}
+            <span className="highlight">
+              driving sales and reducing acquisition costs.
+            </span>
+          </p>
         </div>
+        <div className="future-listbox">
+          <ul className="listbox">
+            <li>
+              <h4>Unlock New Sales and Savings</h4>
+              <p>
+                By introducing Social Buying, witness a surge in sales while
+                minimizing discounts on unsold products.
+              </p>
+            </li>
+            <li>
+              <h4>Personalized Buying Experiences</h4>
+              <p>
+                Offer customers a tailored shopping journey, allowing multiple
+                buyers to unite in a single order.
+              </p>
+            </li>
+            <li>
+              <h4>Insights and Data</h4>
+              <p>
+                Gain invaluable insights into customer networks, enabling
+                precise marketing engagements and a deeper understanding of
+                buyer behavior.
+              </p>
+            </li>
+            <li>
+              <h4>Monetize Existing Data</h4>
+              <p>
+                Maximize the potential of your customer data to enhance revenue
+                streams.
+              </p>
+            </li>
+            <li>
+              <h4>Increased Customer Satisfaction</h4>
+              <p>
+                Provide a seamless co-buying experience, reducing basket
+                abandonment and enhancing satisfaction.
+              </p>
+            </li>
+            <li>
+              <h4>Tech Spend Reduction</h4>
+              <p>
+                Streamline technology costs while boosting order values and
+                reducing acquisition expenses.
+              </p>
+            </li>
+          </ul>
+        </div>
+        {/* </div> */}
       </section>
 
-      <section className="transform">
+      {/* <section className="transform">
         <div className="badge">Checkout Transformation</div>
         <div className="transform-textbox">
           <h2>TRANSFORM YOUR CHECKOUT PROCESS FOR TODAY'S ERA OF SHARING </h2>
           <p className="transform-text">
             Cobuyr gives customers a new and memorable social buying experience
-            that also modernizes your checkout process by <span className="highlight">making co-buying a
-            reality</span> , and creating more value for you.
+            that also modernizes your checkout process by{" "}
+            <span className="highlight">making co-buying a reality</span> , and
+            creating more value for you.
           </p>
         </div>
         <div className="hex-box">
@@ -62,6 +103,28 @@ const Home = () => {
             <li>Increase average order value</li>
             <li>Reduce technology spend</li>
           </ul>
+        </div>
+      </section> */}
+      <section className="modern">
+        <div className="badge">New Method</div>
+        <div className="textbox">
+          <h2>THE MODERN PAYMENT PLATFORM</h2>
+          <p>
+            Introducing Cobuyr, a groundbreaking, comprehensive platform at the
+            forefront of modernized payment solutions. Experience the fusion of
+            payments, data management, and analytics, empowering you to harness
+            the collective purchasing potential of a vast community of cobuyers.
+            These are individuals seeking collaborative ways to split the
+            expenses of online purchases among their social circles,
+            revolutionizing the way products and services are acquired and
+            enjoyed.
+          </p>
+          <div className="image">
+            <img
+              src={dash}
+              alt="new category of multi-party payment transactions"
+            />
+          </div>
         </div>
       </section>
 
@@ -81,28 +144,9 @@ const Home = () => {
         <div className="glow2"></div>
       </section>
 
-      <section className="modern">
-        <div className="badge">New Method</div>
+      {/* <section className="banner2">
         <div className="textbox">
-          <h2>THE MODERN PAYMENTS PLATFORM</h2>
-          <p>
-            Cobuyr is the first and only all-in-one payments, data and analytics
-            platform that enables you to tap into the immense purchasing power
-            of millions of cobuyers - customers who want to share the cost of
-            buying products and services online with family and friends.
-          </p>
-          <div className="image">
-            <img
-              src={dash}
-              alt="new category of multi-party payment transactions"
-            />
-          </div>
-        </div>
-      </section>
-
-      <section className="banner2">
-        <div className="textbox">
-          {/* <h2>TRANSFORM YOUR CHECKOUT PROCESS FOR TODAY'S ERA OF SHARING </h2> */}
+          <h2>TRANSFORM YOUR CHECKOUT PROCESS FOR TODAY'S ERA OF SHARING </h2>
           <p className="banner-text">
             Give your customers the Social Buying experience they expect at
             checkout that will also enhance your payment, data and analytics
@@ -111,19 +155,113 @@ const Home = () => {
           </p>
           <button className="primary-btn">Book Demo</button>
         </div>
-      </section>
+      </section> */}
 
-      <section className="feature">
-        <div className="badge">Features</div>
-        <h2>What Cobuyr Offers</h2>
-        <div className="feature-listbox">
-          <ul className="ft-listbox">
+      <section className="integrate">
+        <div className="int-textbox">
+          {/* <div className="badge">Integrate</div> */}
+          <h2>Integration and Benefits.</h2>
+          <p>
+            Elevate your e-commerce experience with Cobuyr's user-friendly API
+            Plug-In. Our seamlessly integrated solution enables effortless
+            incorporation of Social Buying into your existing website structure,
+            ensuring a smooth and efficient transition without disrupting your
+            operations.
+            <br />
+            <blockquote>
+              By integrating our API, you unlock a world of collaborative
+              purchasing opportunities for your customers while streamlining
+              backend processes for your business.
+            </blockquote>
+          </p>
+          <button className="primary-btn">Documentation</button>
+        </div>
+        <div className=" marquee marquee--vertical">
+          <ul className="marquee__group ">
             <li>Lower the cost of customer acquisition</li>
             <li>Highly personalised buying experience</li>
             <li>Fewer discounts to move unsold products</li>
             <li>Unique insights into customer networks</li>
             <li>Multiple customer buy together in one order</li>
           </ul>
+          <ul aria-hidden="true" className="marquee__group ">
+            <li>Lower the cost of customer acquisition</li>
+            <li>Highly personalised buying experience</li>
+            <li>Fewer discounts to move unsold products</li>
+            <li>Unique insights into customer networks</li>
+            <li>Multiple customer buy together in one order</li>
+          </ul>
+        </div>
+      </section>
+      
+      <section>
+        <div class="container">
+          <div class="head">
+            <div className="badge">The Future</div>
+            <h2>THE FUTURE OF PAYMENTS</h2>
+          </div>
+          <div class="info1">
+            <div className="textbox">
+              <h3>Unified Payment Innovation</h3>
+              <p>
+                Discover Cobuyr, the pioneer in integrated payment platforms.
+                Seamlessly amalgamating payments, data, and analytics, it
+                revolutionizes the purchasing experience. Unlock the collective
+                purchasing power of millions engaged in collaborative buying
+                with family and friends.
+              </p>
+              {/* <div className="image">
+                <img
+                  src={dash}
+                  alt="new category of multi-party payment transactions"
+                />
+              </div> */}
+            </div>
+          </div>
+          <div class="info3">
+          <div className="textbox">
+              <h3>Unified Payment Innovation</h3>
+              <p>
+                Discover Cobuyr, the pioneer in integrated payment platforms.
+                Seamlessly amalgamating payments, data, and analytics, it
+                revolutionizes the purchasing experience. Unlock the collective
+                purchasing power of millions engaged in collaborative buying
+                with family and friends.
+              </p>
+            </div>
+          </div>
+          <div class="info2">
+          <div className="textbox">
+              <h3>Unified Payment Innovation</h3>
+              <p>
+                Discover Cobuyr, the pioneer in integrated payment platforms.
+                Seamlessly amalgamating payments, data, and analytics, it
+                revolutionizes the purchasing experience. Unlock the collective
+                purchasing power of millions engaged in collaborative buying
+                with family and friends.
+              </p>
+            </div>
+          </div>
+          <div class="info-mini">
+          <div className="textbox">
+              <h3>Unified Payment Innovation</h3>
+              <p>
+                Discover Cobuyr
+              </p>
+            </div>
+          </div>
+          <div className="poll">
+          <div className="textbox">
+              <h3>What Ecommerce platform is your favourite?</h3>
+              <p>
+                Discover Cobuyr, the pioneer in integrated payment platforms.
+                Seamlessly amalgamating payments, data, and analytics, it
+                revolutionizes the purchasing experience. Unlock the collective
+                purchasing power of millions engaged in collaborative buying
+                with family and friends.
+              </p>
+            </div>
+          </div>
         </div>
       </section>
 
@@ -150,6 +288,7 @@ const Home = () => {
           .
         </h6>
       </section>
+
     </>
   );
 };
