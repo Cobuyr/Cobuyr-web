43892adc4ee60bc27f331579b26bc884e6aa74bf
zhaqyy <zaqrashyy7@gmail.com>
2023-11-26 17:21:28 +0100

    finihed first about section

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,4 +1,4 @@
-import "../Styles/About.css"
+import "../Styles/About.css";
 import { Badge, IconBadge } from "../Components/badge";
 
 const About = () => {
@@ -6,7 +6,7 @@ const About = () => {
     <>
       <section className="who">
         <div className="who-head">
-        <Badge iconUrl="iuaevrjs" trigger="loop" text="Welcome" />
+          <Badge iconUrl="kddybgok" trigger="hover" text="Who We Are" />
           <h1>
             Cobuyr pioneered Social Buying, redefining e-commerce payments into
             a collaborative experience.
@@ -19,14 +19,18 @@ const About = () => {
             everyday essentials strain budgets and sharing experiences enriches
             lives, we recognized a crucial gap in the e-commerce landscape: the
             inability to facilitate shared payments for products and services.
+            <br />
+            <br />
             Traditional e-commerce methods hinder the collective buying
             experience. Online checkout processes typically cater to
             single-purchase transactions, limiting social dynamics and business
             potential. However, we've brought forth a game-changing solution.
-            Cobuyr pioneered Social Buying, redefining e-commerce payments into
-            a collaborative experience. Our platform empowers families, friends,
-            and communities within marketplaces to unite in group purchases for
-            the first time.
+            <br />
+            <br />
+            <span className="highlight">
+              Our platform empowers families, friends, and communities within
+              marketplaces to unite in group purchases for the first time.
+            </span>
           </p>
         </div>
       </section>
