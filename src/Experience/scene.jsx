311be06c7c94df93d5e6644b4692cd0bbb20254d
zhaqyy <zaqrashyy7@gmail.com>
2023-12-16 13:48:38 +0100

    R3F scene base

---
+++ src/Experience/scene.jsx
@@ -0,0 +1,12 @@
+import { Canvas } from '@react-three/fiber'
+
+const Scene = () => {
+  return (
+    <div id="canvas-container">
+      <Canvas>
+          
+      </Canvas>
+    </div>
+  )
+}
+export default Scene;
