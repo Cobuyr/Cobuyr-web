607d8b0148916b5400d99381640d2912f8e7df83
zhaqyy <zaqrashyy7@gmail.com>
2023-12-20 16:54:42 +0100

    fixed page transition

--- src/Components/Transition.jsx
+++ src/Components/Transition.jsx
@@ -25,30 +25,53 @@
 
 // export default Transition;
 
-import { motion } from "framer-motion";
+import { motion, useIsPresent } from "framer-motion";
 import React from "react";
 
 // Transition HOC
 const Transition = (WrappedComponent) => {
+  // const isPresent = useIsPresent();
+
   return () => (
-    <> 
+    <>
+      <WrappedComponent />
       <motion.div
+        className="slide-in"
+        initial={{
+          opacity: 1,
+          clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+        }}
+        animate={{
+          opacity: 0,
+          clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+          // scaleX:0,
+          transitionEnd: {
+            display: "none",
+          },
+        }}
+        exit={{ clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)" }}
+        transition={{ duration: 0.75}}
+      />
+      {/* <motion.div
         className="slide-in"
         initial={{ scaleY: 1 }}
         animate={{ scaleY: 0 }}
         exit={{ scaleY: 0 }}
-        transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] , delay:0.5}}
-      />
+        transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
+      /> */}
 
-    <WrappedComponent />
-
-      <motion.div
+      {/* <motion.div
         className="slide-out"
-        initial={{ scaleY: 1 }}
-        animate={{ scaleY: 0 }}
-        exit={{ scaleY: 0 }}
-        transition={{ duration: 1, ease: [0.22, 1, 0.36, 1], delay:0.5}}
-      />
+        // initial={{ scaleY: 1 }}
+        // animate={{ scaleY: 0 }}
+        // exit={{ scaleY: 0 }}
+        // transition={{ duration: 1, ease: "circOut", delay:0.5}}
+
+        initial={{ scaleX: 1 }}
+        animate={{ scaleX: 0, transition: { duration: 0.5, ease: "circOut" } }}
+        exit={{ scaleX: 1, transition: { duration: 0.5, ease: "circIn" } }}
+        // style={{ originX: isPresent ? 0 : 1 }}
+      /> */}
     </>
   );
 };
