07bb7d5777553fba5be17252a584d0a100068e1f
zhaqyy <zaqrashyy7@gmail.com>
2024-01-02 11:41:11 +0100

    added environment map, lighting and shadows to scene

--- src/Experience/Experience.jsx
+++ src/Experience/Experience.jsx
@@ -2,9 +2,9 @@
 /* eslint-disable react/no-unknown-property */
 
 import {
+  AccumulativeShadows,
+  RandomizedLight,
   OrbitControls,
-  BakeShadows,
-  ContactShadows,
   Environment,
 } from "@react-three/drei";
 import { Perf } from "r3f-perf";
@@ -21,22 +21,38 @@ export default function Experience() {
       <directionalLight
         castShadow
         position={[0, -5, 0]}
-        intensity={2}
+        intensity={1}
         shadow-normalBias={0.04}
       />
       <directionalLight
         castShadow
         position={[0, 5, 0]}
-        intensity={2}
+        intensity={1}
         shadow-normalBias={0.04}
       />
 
-      <ambientLight intensity={4} />
-     
+      <ambientLight intensity={1} />
 
       <Suspense>
         <CobuyrModel />
-        {/* <Environment preset="night" /> */}
+        <AccumulativeShadows
+          temporal
+          frames={200}
+          color="purple"
+          colorBlend={0.5}
+          opacity={1}
+          scale={10}
+          alphaTest={0.85}
+        >
+          <RandomizedLight
+            amount={8}
+            radius={5}
+            ambient={0.5}
+            position={[5, 3, 2]}
+            bias={0.001}
+          />
+        </AccumulativeShadows>
+        <Environment preset={"night"} background={null} blur={1}   />
       </Suspense>
     </>
   );
