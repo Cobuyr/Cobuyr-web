863a157206dacdcf70b8306ee9c794cd684399e3
zhaqyy <zaqrashyy7@gmail.com>
2024-01-02 18:55:28 +0100

    -added ring rotattion via mouse -added night hdr

--- src/Experience/Experience.jsx
+++ src/Experience/Experience.jsx
@@ -1,26 +1,30 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/no-unknown-property */
 
+import * as THREE from "three";
 import {
   AccumulativeShadows,
   RandomizedLight,
   OrbitControls,
   Environment,
 } from "@react-three/drei";
+import { useFrame, useThree } from "@react-three/fiber";
 import { Perf } from "r3f-perf";
-import { Suspense } from "react";
+import { Suspense, useRef, useEffect } from "react";
+import { easing } from "maath";
 import CobuyrModel from "./CobuyrModel";
 import MetaFloat from "./Metaball";
 import { DoubleSide } from "three";
-import { EffectComposer, Bloom } from '@react-three/postprocessing'
-
+import { EffectComposer, Bloom } from "@react-three/postprocessing";
 
 export default function Experience() {
+  const RingRef = useRef();
+
   return (
     <>
       <Perf position="top-left" />
 
-      <OrbitControls makeDefault />
+      {/* <OrbitControls makeDefault /> */}
 
       <directionalLight
         castShadow
@@ -34,41 +38,67 @@ export default function Experience() {
         intensity={1}
         shadow-normalBias={0.04}
       />
-<EffectComposer>
+      <EffectComposer>
         <Bloom mipmapBlur luminanceThreshold={0.6} radius={0.3} />
       </EffectComposer>
       <ambientLight intensity={1} />
 
-      <mesh rotation={[0, -0.25, 0]}>
-        <ringGeometry args={[1.3, 1.35, 64]} />
-        <meshBasicMaterial
-          color={"pink"} 
-          side={DoubleSide}
-        />
-      </mesh>
-
+      <Ring />
+      
       <Suspense>
-        <CobuyrModel />
-        {/* <AccumulativeShadows
-          temporal
-          frames={200}
-          color="purple"
-          colorBlend={0.5}
-          opacity={1}
-          scale={10}
-          alphaTest={0.85}
-        >
-          <RandomizedLight
-            amount={8}
-            radius={5}
-            ambient={0.5}
-            position={[5, 3, 2]}
-            bias={0.001}
-          />
-        </AccumulativeShadows> */}
-        <Environment preset={"night"} background={null} blur={1} />
+        <CameraRig>
+          <CobuyrModel />
+        </CameraRig>
+
+        <Environment files={"./night.hdr"} background={null} blur={1} />
+        {/* <Rig/> */}
         {/* <MetaFloat/> */}
       </Suspense>
     </>
   );
 }
+
+function CameraRig({ children }) {
+  const group = useRef();
+  // const snap = useSnapshot(state)
+  useFrame((state, delta) => {
+    easing.damp3(
+      state.camera.position,
+      [state.intro ? -state.viewport.width / 4 : 0, 0, 3.5],
+      0.25,
+      delta
+    );
+    easing.dampE(
+      group.current.rotation,
+      [state.pointer.y / 25, -state.pointer.x / 25, 0],
+      0.25,
+      delta
+    );
+  });
+  return <group ref={group}>{children}</group>;
+}
+
+const Ring = () => {
+  const RingRef = useRef();
+  useEffect(() => {
+    RingRef.current.rotation.set(-Math.PI / 0.25, -Math.PI / 2, 0);
+  }, []);
+  useFrame((state, delta) => {
+    const xRotation = state.pointer.x; // Mouse movement along the X-axis
+    const yRotation = state.pointer.y; // Mouse movement along the Y-axis
+
+    const targetRotation = [
+      -Math.PI / 0.1 - yRotation / 2,
+      -Math.PI / 2 - xRotation,
+      0,
+    ];
+    easing.dampE(RingRef.current.rotation, targetRotation, 0.25, delta);
+  });
+
+  return (
+    <mesh rotation={[0, 0, 0]} ref={RingRef}>
+      <ringGeometry args={[1.3, 1.35, 64]} />
+      <meshBasicMaterial color={"pink"} side={DoubleSide} />
+    </mesh>
+  );
+};
