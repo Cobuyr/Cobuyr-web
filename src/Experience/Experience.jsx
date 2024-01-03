3b1c35f7935d2254368a8427f07195d8e1941b32
zhaqyy <zaqrashyy7@gmail.com>
2024-01-03 19:41:36 +0100

    added wobbly sphere

--- src/Experience/Experience.jsx
+++ src/Experience/Experience.jsx
@@ -7,10 +7,14 @@ import {
   RandomizedLight,
   OrbitControls,
   Environment,
+  MeshDistortMaterial,
+  MeshWobbleMaterial,
+  GradientTexture,
+  useCursor,
 } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
 import { Perf } from "r3f-perf";
-import { Suspense, useRef, useEffect } from "react";
+import { Suspense, useRef, useEffect, useState } from "react";
 import { easing } from "maath";
 import CobuyrModel from "./CobuyrModel";
 import MetaFloat from "./Metaball";
@@ -29,25 +33,42 @@ export default function Experience() {
       <directionalLight
         castShadow
         position={[0, -5, 0]}
-        intensity={1}
+        intensity={0.5}
+        // color={"#008080"}
         shadow-normalBias={0.04}
       />
       <directionalLight
         castShadow
         position={[0, 5, 0]}
-        intensity={1}
+        intensity={0.5}
+        // color={"#800080"}
         shadow-normalBias={0.04}
       />
+
+      <pointLight
+      position={[4, 0, 0]}
+      intensity={15}
+      color={"#008090"}
+      />
+
+      <pointLight
+      position={[-4, 0, 0]}
+      intensity={15}
+      color={"#800080"}
+      />
+
       <EffectComposer>
         <Bloom mipmapBlur luminanceThreshold={0.6} radius={0.3} />
       </EffectComposer>
       <ambientLight intensity={1} />
 
-      <Ring />
-      
+     
+
       <Suspense>
         <CameraRig>
           <CobuyrModel />
+          <Ring />
+      <Blob/>
         </CameraRig>
 
         <Environment files={"./night.hdr"} background={null} blur={1} />
@@ -83,7 +104,7 @@ const Ring = () => {
   useEffect(() => {
     RingRef.current.rotation.set(-Math.PI / 0.25, -Math.PI / 2, 0);
   }, []);
-  
+
   useFrame((state, delta) => {
     const xRotation = state.pointer.x; // Mouse movement along the X-axis
     const yRotation = state.pointer.y; // Mouse movement along the Y-axis
@@ -103,3 +124,27 @@ const Ring = () => {
     </mesh>
   );
 };
+
+function Blob() {
+  const ref = useRef();
+  return (
+    <mesh
+    >
+      <sphereGeometry args={[0.25, 16, 16]} />
+      {/* <MeshWobbleMaterial ref={ref} factor={100} metalness={0.5} envMapIntensity={50} wireframe speed={1}>
+        <GradientTexture
+          stops={[0, 0.5, 1]}
+          colors={["#008080", "#800080", "#008000",]}
+          size={100}
+        />
+      </MeshWobbleMaterial> */}
+      <MeshDistortMaterial ref={ref} factor={0} distort={0.7} metalness={0.9} envMapIntensity={50} speed={1}>
+        <GradientTexture
+          stops={[0, 0.5, 1]}
+          colors={["#008080", "#800080", "#008000",]}
+          size={100}
+        />
+      </MeshDistortMaterial>
+    </mesh>
+  );
+}
