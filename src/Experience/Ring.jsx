bc2841a5b098548b920e08764696d491bdf92225
zhaqyy <zaqrashyy7@gmail.com>
2024-01-21 13:29:45 +0100

    revert back to base scroll animation

--- src/Experience/Ring.jsx
+++ src/Experience/Ring.jsx
@@ -72,65 +72,69 @@ export default function Ring() {
   //     ]
   // );
 
-  const animationOrder = {
-    init: 0,
-    ringhide: 0.01,
-    scaleInLeft: 0.1,
-    LtextFadein: 0.25,
-    LtextFadeinEnd: 0.3,
-    branch1End: 0.4,
-    shiftRight: 0.4,
-    RtextFadein: 0.55,
-    RtextFadeinEnd: 0.75,
-    branch2End: 0.75,
-    jumboTextIn: 0.8,
-    jumboTextInEnd: 0.85,
-    PartnerTextin: 1,
-  };
+
+////////////////////////////
+
+  // const animationOrder = {
+  //   init: 0,
+  //   ringhide: 0.01,
+  //   scaleInLeft: 0.1,
+  //   LtextFadein: 0.25,
+  //   LtextFadeinEnd: 0.3,
+  //   branch1End: 0.4,
+  //   shiftRight: 0.4,
+  //   RtextFadein: 0.55,
+  //   RtextFadeinEnd: 0.75,
+  //   branch2End: 0.75,
+  //   jumboTextIn: 0.8,
+  //   jumboTextInEnd: 0.85,
+  //   PartnerTextin: 1,
+  // };
 
   const { scrollYProgress } = useScroll();
-  const yAngle = useTransform(
-    scrollYProgress,
-    [0, 1],
-    [0, degreesToRadians(180)]
-  );
+  // const yAngle = useTransform(
+  //   scrollYProgress,
+  //   [0, 1],
+  //   [0, degreesToRadians(180)]
+  // );
 
-  const Xpos = useTransform(
-    scrollYProgress,
-    [
-      animationOrder.init,
-      animationOrder.ringhide,
-      animationOrder.scaleInLeft,
-      animationOrder.LtextFadein,
-      animationOrder.LtextFadeinEnd,
-      animationOrder.branch1End,
-      animationOrder.shiftRight,
-      animationOrder.RtextFadeinEnd,
-      animationOrder.branch2End,
-      animationOrder.jumboTextIn,
-      animationOrder.PartnerTextin,
-    ],
-    [0, 0, -6, -4.5, -4.5, -6, 4.5, 4.5, 6, 0, 0]
-  );
+  // const Xpos = useTransform(
+  //   scrollYProgress,
+  //   [
+  //     animationOrder.init,
+  //     animationOrder.ringhide,
+  //     animationOrder.scaleInLeft,
+  //     animationOrder.LtextFadein,
+  //     animationOrder.LtextFadeinEnd,
+  //     animationOrder.branch1End,
+  //     animationOrder.shiftRight,
+  //     animationOrder.RtextFadeinEnd,
+  //     animationOrder.branch2End,
+  //     animationOrder.jumboTextIn,
+  //     animationOrder.PartnerTextin,
+  //   ],
+  //   [0, 0, -6, -4.5, -4.5, -6, 4.5, 4.5, 6, 0, 0]
+  // );
   // const Ypos = useTransform(scrollYProgress, [0, 1], [0, -2]);
-  const scale = useTransform(
-    scrollYProgress,
-    [
-      animationOrder.init,
-      animationOrder.ringhide,
-      animationOrder.scaleInLeft,
-      animationOrder.LtextFadein,
-      animationOrder.branch1End,
-      animationOrder.Ltfadeout,
-      animationOrder.shiftRight,      
-      animationOrder.RtextFadeinEnd,
-      animationOrder.branch2End,
-      animationOrder.jumboTextIn,
-      animationOrder.PartnerTextin,
-    ],
-    [1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1]
-  );
+  // const scale = useTransform(
+  //   scrollYProgress,
+  //   [
+  //     animationOrder.init,
+  //     animationOrder.ringhide,
+  //     animationOrder.scaleInLeft,
+  //     animationOrder.LtextFadein,
+  //     animationOrder.branch1End,
+  //     animationOrder.Ltfadeout,
+  //     animationOrder.shiftRight,      
+  //     animationOrder.RtextFadeinEnd,
+  //     animationOrder.branch2End,
+  //     animationOrder.jumboTextIn,
+  //     animationOrder.PartnerTextin,
+  //   ],
+  //   [1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1]
+  // );
 
+  const scale = useTransform(scrollYProgress, [0, 0.05], [1, 0]);
   useFrame((state, delta) => {
     const xRotation = state.pointer.x; // Mouse movement along the X-axis
     const yRotation = state.pointer.y; // Mouse movement along the Y-axis
@@ -143,11 +147,11 @@ export default function Ring() {
     easing.dampE(RingRef.current.rotation, targetRotation, 0.9, delta);
 
     // RingRef.current.rotation.y = yAngle.get();
-    RingRef.current.position.set(Xpos.get(), 0, 0);
+    // RingRef.current.position.set(Xpos.get(), 0, 0);
     RingRef.current.scale.set(scale.get());
     RingRef.current.scale.y = scale.get();
 
-    console.log(RingRef.current.position.x);
+    // console.log(RingRef.current.position.x);
   });
   const isMobile = window.innerWidth < 768;
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -14,7 +14,6 @@ import { useRef } from "react";
 import Scene from "../Experience/scene";
 import Velocity from "../Components/Velocity";
 
-// import LiquidButton from "../Components/magnetBtn";
 
 const visible = {
   opacity: 1,
@@ -47,30 +46,17 @@ const homeVariants = {
 //   FtCover: 1,
 //   end: 1,
 // };
-const animationOrder = {
-  init: 0,
-  scaleInLeft: 0.1,
-  LtextFadein: 0.25,
-  LtextFadeinEnd: 0.3,
-  branch1End: 0.4,
-  Ltfadeout: 0.425,
-  shiftRight: 0.425,
-  RtextFadein: 0.55,
-  RtextFadeinEnd: 0.75,
-  branch2End: 0.75,
-  jumboTextIn: 0.8,
-  jumboTextInEnd: 0.85,
-  PartnerTextin: 1,
-};
 
 const Home = () => {
   return (
     <>
-     <div className="scene">
+         <div className="scene"   
+        //  style={{ position:position }}
+         >
         <Scene />
       </div>
       <Hero />
-      <Scroller />
+      {/* <Scroller /> */}
 
       <Section className="future">
         <motion.div className="future-textbox" variants={homeVariants}>
@@ -524,229 +510,252 @@ const Home = () => {
 const TransHome = Transition(Home);
 export default TransHome;
 
-export const Scroller = () => {
-  const TargetRef = useRef();
-  const { scrollYProgress } = useScroll({
-    target: TargetRef,
-    offset: ["start end", "end start"],
-  });
+// export const Scroller = () => {
+//   const TargetRef = useRef();
+//   const { scrollYProgress } = useScroll({
+//     target: TargetRef,
+//     offset: ["start end", "end start"],
+//   });
 
-  const opacity = useTransform(
-    scrollYProgress,
-    [
-      animationOrder.init,
-      animationOrder.scaleInLeft,
-      animationOrder.LtextFadeinEnd,
-      animationOrder.branch1End,
-      animationOrder.RtextFadeinEnd,
-      animationOrder.branch2End,
-      animationOrder.jumboTextIn,
-    ],
-    [0, 1, 1, 1, 1, 0, 0]
-  );
-  const scale = useTransform(
-    scrollYProgress,
-    [
-      animationOrder.init,
-      animationOrder.scaleInLeft,
-      animationOrder.LtextFadeinEnd,
-      animationOrder.branch1End,
-      animationOrder.RtextFadein,
-    ],
-    [5, 3, 2, 2, 2]
-  );
-  const x = useTransform(
-    scrollYProgress,
-    [
-      animationOrder.init,
-      animationOrder.scaleInLeft,
-      animationOrder.LtextFadeinEnd,
-      animationOrder.branch1End,
-      animationOrder.RtextFadein,
-      animationOrder.RtextFadeinEnd,
-      animationOrder.jumboTextIn,
-    ],
-    ["120%", "100%", "100%", "100%", "-100%", "-100%", "-200%"]
-  );
+//   const animationOrder = {
+//     init: 0,
+//     scaleInLeft: 0.1,
+//     LtextFadein: 0.25,
+//     LtextFadeinEnd: 0.3,
+//     branch1End: 0.4,
+//     Ltfadeout: 0.425,
+//     shiftRight: 0.425,
+//     RtextFadein: 0.55,
+//     RtextFadeinEnd: 0.75,
+//     branch2End: 0.75,
+//     jumboTextIn: 0.8,
+//     jumboTextInEnd: 0.85,
+//     PartnerTextin: 1,
+//   };
+  
+//   const opacity = useTransform(
+//     scrollYProgress,
+//     [
+//       animationOrder.init,
+//       animationOrder.scaleInLeft,
+//       animationOrder.LtextFadeinEnd,
+//       animationOrder.branch1End,
+//       animationOrder.RtextFadeinEnd,
+//       animationOrder.branch2End,
+//       animationOrder.jumboTextIn,
+//     ],
+//     [0, 1, 1, 1, 1, 0, 0]
+//   );
+//   const scale = useTransform(
+//     scrollYProgress,
+//     [
+//       animationOrder.init,
+//       animationOrder.scaleInLeft,
+//       animationOrder.LtextFadeinEnd,
+//       animationOrder.branch1End,
+//       animationOrder.RtextFadein,
+//     ],
+//     [5, 3, 2, 2, 2]
+//   );
+//   const x = useTransform(
+//     scrollYProgress,
+//     [
+//       animationOrder.init,
+//       animationOrder.scaleInLeft,
+//       animationOrder.LtextFadeinEnd,
+//       animationOrder.branch1End,
+//       animationOrder.RtextFadein,
+//       animationOrder.RtextFadeinEnd,
+//       animationOrder.jumboTextIn,
+//     ],
+//     ["120%", "100%", "100%", "100%", "-100%", "-100%", "-200%"]
+//   );
 
-  const T1opacity = useTransform(
-    scrollYProgress,
-    [
-      animationOrder.init,
-      animationOrder.LtextFadein,
-      animationOrder.LtextFadeinEnd,
-      animationOrder.branch1End,
-      animationOrder.Ltfadeout,
-    ],
-    [0, 0, 1, 1, 0]
-  );
-  const T1scale = useTransform(
-    scrollYProgress,
-    [
-      animationOrder.init,
-      animationOrder.LtextFadein,
-      animationOrder.LtextFadeinEnd,
-      animationOrder.branch1End,
-      animationOrder.Ltfadeout,
-    ],
-    [0, 1, 1, 1, 0]
-  );
+//   const T1opacity = useTransform(
+//     scrollYProgress,
+//     [
+//       animationOrder.init,
+//       animationOrder.LtextFadein,
+//       animationOrder.LtextFadeinEnd,
+//       animationOrder.branch1End,
+//       animationOrder.Ltfadeout,
+//     ],
+//     [0, 0, 1, 1, 0]
+//   );
+//   const T1scale = useTransform(
+//     scrollYProgress,
+//     [
+//       animationOrder.init,
+//       animationOrder.LtextFadein,
+//       animationOrder.LtextFadeinEnd,
+//       animationOrder.branch1End,
+//       animationOrder.Ltfadeout,
+//     ],
+//     [0, 1, 1, 1, 0]
+//   );
 
-  const T2opacity = useTransform(
-    scrollYProgress,
-    [
-      animationOrder.init,
-      animationOrder.shiftRight,
-      animationOrder.RtextFadein,
-      animationOrder.RtextFadeinEnd,
-      animationOrder.branch2End,
-      animationOrder.jumboTextIn,
-    ],
-    [0, 0, 1, 1, 0, 0]
-  );
-  const T2scale = useTransform(
-    scrollYProgress,
-    [
-      animationOrder.init,
-      animationOrder.shiftRight,
-      animationOrder.RtextFadein,
-      animationOrder.RtextFadeinEnd,
-      animationOrder.branch2End,
-      animationOrder.jumboTextIn,
-    ],
-    [0, 0, 1, 1, 0, 0]
-  );
-  const Jopacity = useTransform(
-    scrollYProgress,
-    [
-      animationOrder.init,
-      animationOrder.RtextFadeinEnd,
-      animationOrder.jumboTextIn,
-      animationOrder.jumboTextInEnd,
-      animationOrder.PartnerTextin,
-    ],
-    [0, 0, 1, 1, 1]
-  );
-  const Jscale = useTransform(
-    scrollYProgress,
-    [
-      animationOrder.init,
-      animationOrder.RtextFadeinEnd,
-      animationOrder.jumboTextIn,
-      animationOrder.jumboTextInEnd,
-      0.87,
-      animationOrder.PartnerTextin,
-    ],
-    [0, 0.5, 1, 1, 1, 1]
-  );
+//   const T2opacity = useTransform(
+//     scrollYProgress,
+//     [
+//       animationOrder.init,
+//       animationOrder.shiftRight,
+//       animationOrder.RtextFadein,
+//       animationOrder.RtextFadeinEnd,
+//       animationOrder.branch2End,
+//       animationOrder.jumboTextIn,
+//     ],
+//     [0, 0, 1, 1, 0, 0]
+//   );
+//   const T2scale = useTransform(
+//     scrollYProgress,
+//     [
+//       animationOrder.init,
+//       animationOrder.shiftRight,
+//       animationOrder.RtextFadein,
+//       animationOrder.RtextFadeinEnd,
+//       animationOrder.branch2End,
+//       animationOrder.jumboTextIn,
+//     ],
+//     [0, 0, 1, 1, 0, 0]
+//   );
+//   const Jopacity = useTransform(
+//     scrollYProgress,
+//     [
+//       animationOrder.init,
+//       animationOrder.RtextFadeinEnd,
+//       animationOrder.jumboTextIn,
+//       animationOrder.jumboTextInEnd,
+//       animationOrder.PartnerTextin,
+//     ],
+//     [0, 0, 1, 1, 1]
+//   );
+//   const Jscale = useTransform(
+//     scrollYProgress,
+//     [
+//       animationOrder.init,
+//       animationOrder.RtextFadeinEnd,
+//       animationOrder.jumboTextIn,
+//       animationOrder.jumboTextInEnd,
+//       0.87,
+//       animationOrder.PartnerTextin,
+//     ],
+//     [0, 0.5, 1, 1, 1, 1]
+//   );
 
-  // const Popacity = useTransform(
-  //   scrollYProgress,
-  //   [
-  //     animationOrder.init,
-  //     animationOrder.PartnerTextin,
-  //     animationOrder.PartnerTextinEnd,
-  //     animationOrder.PartnerScrollvelocity,
-  //     animationOrder.FtCover,
-  //   ],
-  //   [0, 0, 1, 1, 0]
-  // );
-  // const Pscale = useTransform(
-  //   scrollYProgress,
-  //   [
-  //     animationOrder.init,
-  //     animationOrder.PartnerTextin,
-  //     animationOrder.PartnerTextinEnd,
-  //     animationOrder.PartnerScrollvelocity,
-  //     animationOrder.FtCover,
-  //   ],
-  //   [0, 0, 1, 1, 0]
-  // );
+//   // const Popacity = useTransform(
+//   //   scrollYProgress,
+//   //   [
+//   //     animationOrder.init,
+//   //     animationOrder.PartnerTextin,
+//   //     animationOrder.PartnerTextinEnd,
+//   //     animationOrder.PartnerScrollvelocity,
+//   //     animationOrder.FtCover,
+//   //   ],
+//   //   [0, 0, 1, 1, 0]
+//   // );
+//   // const Pscale = useTransform(
+//   //   scrollYProgress,
+//   //   [
+//   //     animationOrder.init,
+//   //     animationOrder.PartnerTextin,
+//   //     animationOrder.PartnerTextinEnd,
+//   //     animationOrder.PartnerScrollvelocity,
+//   //     animationOrder.FtCover,
+//   //   ],
+//   //   [0, 0, 1, 1, 0]
+//   // );
 
-  return (
-    <>
-      <motion.section className="scrollwrap" ref={TargetRef}>
-        <div className="branch1wrap">
-          <motion.h2
-            className="b1Text"
-            style={{ opacity: T1opacity, scale: T1scale }}
-          >
-            Fully Customizable dashboard
-          </motion.h2>
-          <motion.div className="image" style={{ opacity, scale, x }}>
-            <img
-              src={dash}
-              alt="new category of multi-party payment transactions"
-            />
-          </motion.div>
-          <motion.h2
-            className="b1Text"
-            style={{ opacity: T2opacity, scale: T2scale }}
-          >
-            Extremely Detailed Analytics and product tracking
-          </motion.h2>
-        </div>
+//   const position = useTransform(scrollYProgress, (pos)=>
+//   pos >= 0.5 ? "relative" : "fixed" )
+//   return (
+//     <>
+//       <motion.section className="scrollwrap" ref={TargetRef}>
+//       <div className="scene"   style={{ position:position }}>
+//         <Scene />
+//       </div>
+//         <div className="branch1wrap">
+//           <div className="branch1">
+//           <motion.h2
+//             className="b1Text"
+//             style={{ opacity: T1opacity, scale: T1scale }}
+//           >
+//             Fully Customizable dashboard
+//           </motion.h2>
+//           <motion.div className="image" style={{ opacity, scale, x }}>
+//             <img
+//               src={dash}
+//               alt="new category of multi-party payment transactions"
+//             />
+//           </motion.div>
+//           <motion.h2
+//             className="b1Text"
+//             style={{ opacity: T2opacity, scale: T2scale }}
+//           >
+//             Extremely Detailed Analytics and product tracking
+//           </motion.h2>
+//           </div>
+//         </div>
 
-        <div className="branch2wrap">
-          <motion.h1 style={{ opacity: Jopacity, scale: Jscale }}>
-            Joint Purchase <br /> for all your customers...
-          </motion.h1>
-        </div>
+//         <div className="branch2wrap">
+//           <motion.h1 style={{ opacity: Jopacity, scale: Jscale }}>
+//             Joint Purchase <br /> for all your customers...
+//           </motion.h1>
+//         </div>
 
-        {/* <div className="branch3wrap">
-        <motion.h2 style={{ opacity: Popacity, scale: Pscale }}>
-          Quick and easy Integration with any E-commerce Platform.
-        </motion.h2>
-        {/* <Velocity/> */}
-        {/* <motion.div className="partner">
-          <div className=" marquee">
-            <ul className="marquee__group ">
-              <li>
-                <Icon name={"shopify"} />
-              </li>
-              <li>
-                <Icon name={"woo"} />
-              </li>
-              <li>
-                <Icon name={"wix"} />
-              </li>
-              <li>
-                <Icon name={"magento-full"} />
-              </li>
-              <li>
-                <Icon name={"salesforce"} />
-              </li>
-              <li>
-                <Icon name={"paypal"} />
-              </li>
-            </ul>
-            <ul aria-hidden="true" className="marquee__group ">
-              <li>
-                <Icon name={"shopify"} />
-              </li>
-              <li>
-                <Icon name={"woo"} />
-              </li>
-              <li>
-                <Icon name={"wix"} />
-              </li>
-              <li>
-                <Icon name={"magento-full"} />
-              </li>
-              <li>
-                <Icon name={"salesforce"} />
-              </li>
-              <li>
-                <Icon name={"paypal"} />
-              </li>
-            </ul>
-          </div>
-        </motion.div>
-      </div> */}
-      </motion.section>
-    </>
-  );
-};
+//         {/* <div className="branch3wrap">
+//         <motion.h2 style={{ opacity: Popacity, scale: Pscale }}>
+//           Quick and easy Integration with any E-commerce Platform.
+//         </motion.h2>
+//         {/* <Velocity/> */}
+//         {/* <motion.div className="partner">
+//           <div className=" marquee">
+//             <ul className="marquee__group ">
+//               <li>
+//                 <Icon name={"shopify"} />
+//               </li>
+//               <li>
+//                 <Icon name={"woo"} />
+//               </li>
+//               <li>
+//                 <Icon name={"wix"} />
+//               </li>
+//               <li>
+//                 <Icon name={"magento-full"} />
+//               </li>
+//               <li>
+//                 <Icon name={"salesforce"} />
+//               </li>
+//               <li>
+//                 <Icon name={"paypal"} />
+//               </li>
+//             </ul>
+//             <ul aria-hidden="true" className="marquee__group ">
+//               <li>
+//                 <Icon name={"shopify"} />
+//               </li>
+//               <li>
+//                 <Icon name={"woo"} />
+//               </li>
+//               <li>
+//                 <Icon name={"wix"} />
+//               </li>
+//               <li>
+//                 <Icon name={"magento-full"} />
+//               </li>
+//               <li>
+//                 <Icon name={"salesforce"} />
+//               </li>
+//               <li>
+//                 <Icon name={"paypal"} />
+//               </li>
+//             </ul>
+//           </div>
+//         </motion.div>
+//       </div> */}
+//       </motion.section>
+//     </>
+//   );
+// };
 
 export function Hero() {
   const heroRef = useRef(null);
