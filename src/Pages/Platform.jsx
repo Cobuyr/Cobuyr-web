4a98e501d798a2fba87fe033e7b6f48b990fbcff
zhaqyy <zaqrashyy7@gmail.com>
2024-04-09 16:42:14 +0100

    -Platform page completed -header responsiveness fixed -minor tweaks and fixes

--- src/Pages/Platform.jsx
+++ src/Pages/Platform.jsx
@@ -32,12 +32,14 @@ const Platform = () => {
       > */}
       <Section className="platHead">
         <div className="plattext">
-          <h2>modernize platform </h2>
+          <h2>Modernize your checkout overnight to attract and
+retain more customers</h2>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti
-            totam officiis accusamus ea sunt asperiores et molestias dolore,
-            quas quidem culpa impedit laborum sed quae eaque veniam ullam ipsa
-            molestiae?
+            Cobuyr is the first and only all-in-one payments, connected data and
+            customer analytics platform that empowers retailers to acquire more
+            customers, increase average order value, and lower the cost of
+            customer acquisition by enabling people to buy together for the
+            first time.
           </p>
           <SpotBtn text={"Book Demo"} /* variant={homeVariants} */ />
         </div>
@@ -47,23 +49,30 @@ const Platform = () => {
       </Section>
 
       <Section className="info-banner">
-        <h2>Lorem ipsum dolor sit.</h2>
+        <h2>Give your customers the buying experience they want</h2>
         <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam,
-          optio excepturi. Possimus voluptate beatae accusantium excepturi
-          labore quo aspernatur animi?
+          Cobuyr provides the checkout experience consumers expect that will
+          drive and improve customer acquisition and conversion for retailers.
+          Developed for the modern era of Social Buying, Cobuyr transforms your
+          traditional single person payment transaction into a multi-person,
+          money-making group purchasing experience by enabling many people to
+          buy together for the first time at checkout.
         </p>
       </Section>
 
       <Section className="platList">
-        <h2>Lorem ipsum dolor sit amet consectetur.</h2>
+        <h2>Powerful Capabilities in One Solution</h2>
 
         <div className="list">
           <div className="subtext">
-            <h3>Lorem, ipsum dolor.</h3>
+            <h3>Enable customers to split purchases</h3>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam ut,
-              accusamus vitae tempore recusandae nisi.
+              Cobuyr's pre-built API Plug-ln enables you to add the Group Buy
+              form to your website, and within your existing check in process,
+              in just minutes so your customers can start to buy together today.
+              It's the quick and simple to provide your customers with the
+              modern checkout experience they expect including the ability to
+              communicate with each other at payment.
             </p>
           </div>
           <div /* className="col-bg" */ className="platimg">
@@ -73,10 +82,60 @@ const Platform = () => {
 
         <div className="list">
           <div className="subtext">
-            <h3>Lorem, ipsum dolor.</h3>
+            <h3>Give your custorners multiple ways to buy</h3>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam ut,
-              accusamus vitae tempore recusandae nisi.
+              Cobuyr enables your customers to use multiple cards in a single
+              payment transaction by using the Cobur Group Buy checkout form at
+              checkout. Customers can use the group buy functionality to split
+              purchases, split payment cards and to securely share payment links
+              through email, social media and text messaging.
+            </p>
+          </div>
+          <div /* className="col-bg" */ className="platimg">
+            <img src="/group.webp" alt="" />
+          </div>
+        </div>
+        <div className="list">
+          <div className="subtext">
+            <h3>Integrate your systems and data in minutes</h3>
+            <p>
+              Cobuyr automatically integrates your payments, CRM and e-commerce
+              systems using pre- built APIs, as well automating payments,
+              fulfilment and marketing processes between Cobuyr and your systems
+              so there is a seamless flow of data and information, and one
+              unified experience for your customers and employees.
+            </p>
+          </div>
+          <div /* className="col-bg" */ className="platimg">
+            <img src="/group.webp" alt="" />
+          </div>
+        </div>
+        <div className="list">
+          <div className="subtext">
+            <h3>Transform your payments data into customer insights</h3>
+            <p>
+              Cobuy unifies all of your data including single and multi-party
+              payment transactions, customer information, products and much more
+              into a single view so you have a complete understanding of who is
+              buying with whom, when and where. Using Cobuyr's pre-built, auto
+              populated dashboards and reports, you have connected insights to
+              supercharge your marketing and communications strategy.
+            </p>
+          </div>
+          <div /* className="col-bg" */ className="platimg">
+            <img src="/group.webp" alt="" />
+          </div>
+        </div>
+        <div className="list">
+          <div className="subtext">
+            <h3>Predict what your customers will buy next</h3>
+            <p>
+              Cobuyr features advanced analytics which enable you to identify
+              which of your customers are the most valued by volume of
+              transactions and customer lifetime value and, uniquely, which of
+              your connected customers are purchasing together within groups of
+              cobuyers and which of these cobuyers are primary and secondary
+              buyers.
             </p>
           </div>
           <div /* className="col-bg" */ className="platimg">
